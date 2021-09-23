e01b1592975727d7446dfc31e35d2591006a57a1
Andrii Mamchur <a.s.mamchur@gmail.com>
2021-09-24 01:19:34 +0300

    Module-1 Task-2. added comment-question

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -10,6 +10,8 @@ function print(text) {
  * чтобы функция sayHello работала корректно
  */
 function isValid(name) {
+  //Константин, в зависимости где вставлю выражение name === null, тесты могут упасть из-за length метода
+  // (он не применяется с null). это норм или нужно писать по другому?
   if (name === null || name.length < 4 || !name || name.includes(' ')) {
     return false;
   } else {
