0a2d5c037a33d89d9ad10b9958cbf26a57f2cb6a
Andrii Mamchur <a.s.mamchur@gmail.com>
2021-10-16 20:14:55 +0300

    deleted getFemales function using reduce method

--- 3-module/6-task/index.js
+++
@@ -1,8 +0,0 @@
-function getFemales(users) {
-  return users.reduce((females, item) => {
-    if (item.gender == 'f') {
-      females.push(`${item.firstName} ${item.lastName}`);
-    }
-    return females;
-  }, []);
-}
