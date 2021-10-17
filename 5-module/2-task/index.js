5413455851473993c7a0c428c8717403bdf1e030
Andrii Mamchur <a.s.mamchur@gmail.com>
2021-10-18 01:29:35 +0300

    Module 5

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,7 @@
 function toggleText() {
-  // ваш код...
+  const button = document.querySelector('.toggle-text-button');
+  button.addEventListener('click', () => {
+    const text = document.querySelector('#text');
+    text.hidden ? text.hidden = false : text.hidden = true;
+  });
 }
