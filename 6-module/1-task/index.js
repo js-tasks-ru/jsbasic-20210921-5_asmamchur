903e783ae735ba594bd411765279e494b3802993
Andrii Mamchur <a.s.mamchur@gmail.com>
2021-10-24 22:57:36 +0300

    Module 6

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,29 @@
  */
 export default class UserTable {
   constructor(rows) {
+    this.elem = document.createElement('table');
+    this.elem.insertAdjacentHTML('afterbegin', `
+       <thead>
+        <tr>
+          <th>Имя</th>
+          <th>Возраст</th>
+          <th>Зарплата</th>
+          <th>Город</th>
+          <th></th>
+        </tr>
+      </thead>
+    `);
+
+    let tableBody = rows.map(array => {
+      return Object.values(array).map(itemTh => {
+        return `<th>${itemTh}</th>`;
+      }).join('').concat(`<th><button>X</button></th>`);
+    }).map(itemTr => `<tr>${itemTr}</tr>`).join('');
+    
+    this.elem.insertAdjacentHTML('beforeend', `<tbody>${tableBody}</tbody>`);
+
+    this.elem.addEventListener('click', event => {
+      event.target.tagName !== 'BUTTON' || event.target.closest('tr').remove();
+    });
   }
 }
